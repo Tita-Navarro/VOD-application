ed795d2fa3ed352ccf24aac29fa6152565756852
Tita-Navarro <tita1089@gmail.com>
2019-11-21 15:33:43 -0600

    adding some parts into the page and publish

---
+++ videotizame/src/sections/History.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar'
+
+const History =()=>{
+    return(
+        <div className='home-container'>
+            <Navbar/>
+        </div>
+    )
+}
+
+export default History;
