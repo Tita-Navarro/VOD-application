cdfe48d6644b247d0a64a22556ace13b02e77811
Tita-Navarro <tita1089@gmail.com>
2019-11-21 15:30:37 -0600

    Updates

---
+++ precache-manifest.ccdc0b3961a87e500f190873f532a608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23838f73604bb9d0202008e9cece1ef",
+    "url": "/VOD-application/index.html"
+  },
+  {
+    "revision": "fed3560230a6a07b1822",
+    "url": "/VOD-application/static/css/main.a21c28e0.chunk.css"
+  },
+  {
+    "revision": "81fb2b7b04ac5cc68431",
+    "url": "/VOD-application/static/js/2.73c82c7b.chunk.js"
+  },
+  {
+    "revision": "fed3560230a6a07b1822",
+    "url": "/VOD-application/static/js/main.1c8d5aca.chunk.js"
+  },
+  {
+    "revision": "288df9f3b22b24ddcb83",
+    "url": "/VOD-application/static/js/runtime-main.5e042229.js"
+  },
+  {
+    "revision": "a2b389efd303e03a2aee7851f2dbe981",
+    "url": "/VOD-application/static/media/VOD-app.a2b389ef.png"
+  }
+]);
